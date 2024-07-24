1e8b0b15e437e3a639ad3da65a90808dfde4629c
delm83 <delm83@googlemail.com>
2024-07-24 20:02:32 +0100

    completed bar chart

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -12,28 +12,32 @@ df['overweight'] = np.where(df['weight'].values/(df['height'].values/100)**2 > 2
 # 3 normalized data by changing cholestorol & gluc values to 1 (bad) if current value is > 1 else 0 (good)
 df[['cholesterol', 'gluc']] = np.where(df[['cholesterol', 'gluc']].values > 1, 1, 0)
 
+#cleaning data by filtering out incorrect data
+df = df[df['ap_lo'] <= df['ap_hi']]
+df = df[df['height'] >= df['height'].quantile(0.025)]
+df = df[df['height'] <= df['height'].quantile(0.975)]
+df = df[df['weight'] >= df['weight'].quantile(0.025)]
+df = df[df['weight'] <= df['weight'].quantile(0.975)]
+
 # 4
 def draw_cat_plot():
-    # 5 created new dataframe for cat plot
-    df_cat = pd.melt(df, id_vars=['active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke', 'cardio'])
+    # 5 created new dataframe for cat plot in long format
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke'])
 
 
     # 6 grouped and reformatted data and calculated counts
-    df_cat = df_cat.groupby(['cardio', 'active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke' ]).size().reset_index(name='counts')
+    df_cat = df_cat.groupby(['variable','cardio','value'])['variable'].count().reset_index(name='total')
     
+    # 7 prouced chart using seaborn catplot method
+    chart = sns.catplot(x='variable', y='total', hue='value', kind='bar', col='cardio', data=df_cat, errorbar=None)
 
-    # 7 converted data into long format and prouced chart using seaborn catplot method
-    df_cat = pd.melt(df_cat, id_vars=['cardio', 'counts'], value_vars=['active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke'])
-    chart = sns.catplot(x='variable', y='counts', hue='value', kind='bar', col='cardio', data=df_cat)
-
-    # 8  Stored output in fig variable
+    #8  Stored output in fig variable
     
     fig = chart
 
 
     # 9
     fig.savefig('catplot.png')
-    print(df_cat)
     return df
 
 
